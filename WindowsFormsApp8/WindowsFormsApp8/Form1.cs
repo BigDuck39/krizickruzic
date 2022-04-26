a5ccb14281df4318fd24d88b60dd9203f92780c5
PotaToni <61621284+PotaToni@users.noreply.github.com>
2022-04-26 10:48:58 +0200

    Add files via upload

---
+++ WindowsFormsApp8/WindowsFormsApp8/Form1.cs
@@ -0,0 +1,69 @@
+﻿using SimpleTCP;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp8
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        SimpleTcpServer server;
+
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            server = new SimpleTcpServer();
+            server.Delimiter = 0x13;
+            server.StringEncoder = Encoding.UTF8;
+            server.DataReceived += DataReceived; 
+
+        }
+
+        private void DataReceived(object sender, SimpleTCP.Message e)
+        {
+            server.Broadcast(e.MessageString);
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox3.Text += "Server Starting to Communicate";
+            IPAddress ip = IPAddress.Parse(textBox1.Text);
+            server.Start(ip, Convert.ToInt32(textBox2.Text));
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (server.IsStarted)
+            {
+                server.Stop();
+            }
+        }
+
+
+    }
+}
